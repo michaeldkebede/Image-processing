00f0559320f20200df44da75d9b3051fa6168f07
Michael Dereje K <michaeldkebede@gmail.com>
2021-11-18 23:02:35 +0300

    Build basic GUI for app. Include an example png image for example purposes.

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+# A multipurpose image processing program with Tkinter GUI.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import tkinter as tk
 
+window = tk.Tk()
+txt = tk.Label(text="Python rocks!")
+txt.pack()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+image = tk.PhotoImage(file='download.png')
+txt['image'] = image
+window.mainloop()
